b87d561c6727148dbf9fd60a791605209e60bcda
Sümeyye SARAY <148757024+sumeyyesaray@users.noreply.github.com>
2024-02-24 16:20:07 +0300

    Add files via upload

---
+++ Course.java
@@ -0,0 +1,32 @@
+public class Course {
+    Teacher courseTeacher;
+    String name;
+    String code;
+    String prefix;
+    int note;
+
+    public Course(String name, String code, String prefix) {
+        this.name = name;
+        this.code = code;
+        this.prefix = prefix;
+        this.note = 0;
+    }
+
+    public void addTeacher(Teacher t) {
+        if (this.prefix.equals(t.branch)) {
+            this.courseTeacher = t;
+            System.out.println("Operation succesful");
+        } else {
+            System.out.println(t.name + " of the academician can not teach this course.");
+        }
+    }
+
+    public void printTeacher() {
+        if (courseTeacher != null) {
+            System.out.println(this.name + " of academician  : " + courseTeacher.name);
+        } else {
+            System.out.println(this.name + "  Academician is not assigned to the course.");
+        }
+    }
+}
+
